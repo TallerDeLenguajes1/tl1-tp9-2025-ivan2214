93904e61654c2befa78077b2d961e59819cf7d21
ivan2214 <bongiovanniivan12@gmail.com>
2025-06-10 16:11:38 -0300

    añado funcionalidad de agregar csv

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -30,8 +30,9 @@ class Program
             // obtengo todos los directorios osea las carpetas dentro del path que dio el usuario
             string[] directorios = Directory.GetDirectories(path);
             foreach (string dir in directorios)
-            {// itero sobre las carpetas que existen en el path
-                string carpetaNombre = Path.GetFileName(dir);
+            {// itero sobre las carpeta1s que existen en el path
+                FileInfo info = new FileInfo(dir);
+                string carpetaNombre = info.Name;
                 // saco el nombre de las carpetas y lo muestro
                 Console.WriteLine($"- {carpetaNombre}");
             }
@@ -42,13 +43,46 @@ class Program
             string[] archivos = Directory.GetFiles(path);
             foreach (string archivo in archivos)
             {
-                string archivoNombre = Path.GetFileName(archivo);
+                FileInfo info = new FileInfo(archivo);
+                string archivoNombre = info.Name;
                 double tamKb = (double)new FileInfo(archivo).Length / 1024;
 
                 Console.WriteLine($"- {archivoNombre} tamaño: ({tamKb} KB)");
             }
+
+
+            // Geenero el csv
+
+            string nombreCSV = "reporte_archivos.csv";
+
+            GenerarCSV(nombreCSV, archivos, path);
+
+
+
+
         }
 
+
+        void GenerarCSV(string nombreCSV, string[] archivos, string path)
+        {
+            List<string> lineasCSV = new List<string>();
+            lineasCSV.Add("Nombre,Tamaño(KB),UltimaModificacion");
+
+            foreach (string archivo in archivos)
+            {
+                FileInfo info = new FileInfo(archivo);
+                string archivoNombre = info.Name;
+                double tamKb = info.Length / 1024;
+                DateTime ultimaModificacion = info.LastWriteTime;
+
+                lineasCSV.Add($"{archivoNombre},{tamKb},{ultimaModificacion}");
+            }
+
+            string rutaCSV = Path.Combine(path, nombreCSV);
+
+            File.WriteAllLines(rutaCSV, lineasCSV);
+
+        }
 
 
     }
